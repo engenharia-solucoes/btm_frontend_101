fcdc8a227919c2678c581dc3185262f3cc62f8dd
samara-bidweb <samara.silvia@bidweb.com.br>
2023-07-14 11:39:35 -0300

    Page Hero - Landing Page Basic - created and Routes Configured

--- src/components/pages/Hero.jsx
+++ src/components/pages/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
   return (
     <section
       id='home'
-      className='lg:h-[85vh] flex items-center bg-primary lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden'
+      className='lg:h-[85vh] w-screen flex items-center bg-primary lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden'
     >
       <div className='container mx-auto h-full'>
         <div className='flex items-center h-full pt-8'>
