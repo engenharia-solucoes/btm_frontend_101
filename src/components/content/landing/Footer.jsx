36da29469b524301e98cdd7a550edfeba207987c
lucasbidweb <lucas.oliveira@bidweb.com.br>
2023-07-18 10:26:39 -0300

    insert products landing page

--- src/components/content/landing/Footer.jsx
+++ src/components/content/landing/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => (
       <div className="flex-[1] flex flex-col justify-start mr-10">
         <img
           src={logo}
-          alt="hoobank"
+          alt="battoumanager"
           className="w-[266px] h-[72.14px] object-contain"
         />
         <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
--- src/components/content/landing/Testimonials.jsx
+++ src/components/content/landing/Testimonials.jsx
@@ -11,12 +11,6 @@ const Testimonials = () => (
       <h2 className={styles.heading2}>
         O que nossos clientes <br className="sm:block hidden" /> falam sobre nosso serviço
       </h2>
-      <div className="w-full md:mt-0 mt-6">
-        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-          Everything you need to accept card payments and grow your business
-          anywhere on the planet.
-        </p>
-      </div>
     </div>
 
     <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
