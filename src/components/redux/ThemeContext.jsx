3c8ace20fc46b1428d628aab85e96f0106ecb6a6
samara-bidweb <samara.silvia@bidweb.com.br>
2023-07-18 11:29:12 -0300

    Updated Test DarkMode 1

--- src/components/redux/ThemeContext.jsx
+++ src/components/redux/ThemeContext.jsx
@@ -3,6 +3,7 @@ import React, { createContext, useState } from 'react';
 const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
+  
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const toggleDarkMode = () => {
